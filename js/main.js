c5c6574e08850ba21fa65a5c7bfaec9b8627fa50
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2025-09-04 16:02:23 -0700

    Change next arrow functionality to strictly next page

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 let sections = Array.from(document.querySelectorAll("section[id]"))
-const headerOffset = 0;
 
-function currentSectionByTop(offset = 0){
-    for(let i =0; i<sections.length; i++){
-        if(sections[i].getBoundingClientRect().top >= -offset) return i;
-    }
-    return sections.length - 1;
+function getOwnerSection(clickedElement){
+    return clickedElement.closest("section")
 }
 
-function scrollToNextSection(){
-    const currentSection = currentSectionByTop(headerOffset);
-    const nextIndex = Math.min(currentSection + 1, sections.length - 1);
-    sections[nextIndex].scrollIntoView({ behavior: "smooth", block: "start"});
+function scrollToNextSection(event){
+    const button = event.currentTarget
+    const ownerSection = getOwnerSection(button)
+    let currentSection = sections.indexOf(ownerSection)
+
+    let nextSection = Math.min(currentSection + 1, sections.length - 1);
+    sections[nextSection].scrollIntoView({ behavior: "smooth", block: "start"});
 
 }
 
-document.getElementById("intro-next-button").addEventListener("click", scrollToNextSection)
+let nextBtn = document.getElementsByClassName("next-button")
+for(let element of nextBtn){
+    element.addEventListener("click", scrollToNextSection)
+}
+    
