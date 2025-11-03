8d6bd071031fe65d153c772f8623fbd87d0e3023
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2025-11-02 22:09:25 -0800

    Add beginning of about me section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,8 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 import meImg from '../assets/me-portrait.jpg'
+import viewImg from '../assets/cowellview.jpg'
+import beachImg from '../assets/meonthebeach.jpeg'
 
 export default function Hero(){
     useEffect(() =>{
@@ -11,7 +13,7 @@ export default function Hero(){
         <div className="hero bg-[#0a2a4a] h-50 w-">
             <div className="space-y-1 p-10 bg-[#0c1a2b] text-white min-h-screen">
                 <div data-aos="zoom-in" className="bg-[#0b233b] p-8 rounded-xl flex flex-col shadow-xl h-200">
-                    <h2 className=" flex text-2xl font-bold text-center bg-[#112840] mb-5 rounded-2xl h-15 justify-center items-center shadow-2xl">Hey there, im <span className="text-[#2289e3]">&nbsp;Michael</span></h2>
+                    <h2 className=" flex text-2xl font-bold text-center bg-[#112840] mb-5 rounded-2xl h-15 justify-center items-center shadow-2xl">Hey there, im <span className="text-[#2289e3]">&nbsp;Michael!</span></h2>
                     <div className="flex justify-between">
                         <div data-aos="zoom-in-right" className="flex justify-start text-lg text-left bg-[#112840] rounded-2xl w-80 p-5 hidden md:flex shadow-xl">
                             Welcome to my site! I'm Michael
@@ -20,16 +22,22 @@ export default function Hero(){
                             I love the challenge of making AI accessible through clean, intuitive interfaces.</div>
                         <img data-aos="zoom-in-up" src={meImg} alt="photo of me" className="w-75 h-75 lg:75 lg:75 rounded-2xl shadow-xl"/>
                     </div>
+                    <div>
+                        <img data-aos="fade-up-left" className="mt-5 rounded-2xl shadow-2xl" src={viewImg} alt="UCSC campus view" />
+                    </div>
                 </div>
             <div data-aos="zoom-in" className="bg-[#0b233b] p-8 rounded-xl flex flex-col shadow-xl h-200">
-                    <h2 className=" flex text-2xl font-bold text-center bg-[#112840] mb-5 rounded-2xl h-15 justify-center items-center shadow-2xl">Hey there, im <span className="text-[#2289e3]">&nbsp;Michael</span></h2>
+                    <h2 className=" flex text-2xl font-bold text-center bg-[#112840] mb-5 rounded-2xl h-15 justify-center items-center shadow-2xl">Here's a little <span className="text-[#2289e3]">&nbsp;About Me:</span></h2>
                     <div className="flex justify-between">
-                        <div data-aos="zoom-in-right" className="flex justify-start text-lg text-left bg-[#112840] rounded-2xl w-80 p-3 hidden md:flex shadow-xl">
-                            Welcome to my site! I'm Michael
-                            I'm a computer science student at UC Santa Cruz fascinated by how machines understand and process language.
-                            Currently working on RAG projects and building full-stack applications.
-                            I love the challenge of making AI accessible through clean, intuitive interfaces.</div>
-                        <img data-aos="zoom-in-up" src={meImg} alt="photo of me" className="w-67 h-67 lg:67 lg:67 rounded-4xl shadow-xl"/>
+                        <div data-aos="zoom-in-right" className="flex justify-start text-md text-left bg-[#112840] rounded-2xl w-80 p-3 hidden md:flex shadow-xl">
+                           I'm a computer science student at UC Santa Cruz with a passion for artificial intelligence and natural language processing. There's something fascinating about teaching machines to understand and 
+                           generate human language—it's like building a bridge between human thought and computational power.
+                            Right now, I'm working on two RAG projects that I'm really excited about. RTFM helps communities avoid answering the same questions over and over by retrieving previous responses—perfect for
+                            Discord servers, forums, or any place where "this has been answered before" is a common gripe. I'm also working on Smart Plate, 
+                            an AI-powered nutrition tracking system that makes logging meals and getting fitness advice smarter and more intuitive.
+                            When I'm not training models or debugging code, I'm building full-stack web applications with React and Tailwind CSS. 
+                        </div>
+                        <img data-aos="zoom-in-up" src={beachImg} alt="photo of me" className="w-75 h-100 lg:67 lg:67 rounded-4xl shadow-xl"/>
                     </div>
                 </div>
             </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 export default function Navbar(){
     return(
         <>
-        <div className="fixed z-10 top-2 w-[100%] flex justify-center"> 
-            <ul className="flex flex-row justify-center h-13 w-[85%] items-center bg-[#081624] gap-5 rounded-2xl shadow-xl">
-                <li><a href=""><button className="hover:text-[white] hover:text-xl hover:bg-[#073761] bg-[#081b2e] cursor-pointer lg:rounded-xl rounded-md h-8 lg:w-30 lg:text-lg font-bold transition-all duration-200 ease-in-out w-20">Home</button></a></li>
-                <li><a href=""><button className="hover:text-[white] hover:text-xl hover:bg-[#073761] bg-[#081b2e] cursor-pointer lg:rounded-xl rounded-md h-8 lg:w-30 lg:text-lg font-bold transition-all duration-200 ease-in-out w-20">About</button></a></li>
-                <li><a href=""><button className="hover:text-[white] hover:text-xl hover:bg-[#073761] bg-[#081b2e] cursor-pointer lg:rounded-xl rounded-md h-8 lg:w-30 lg:text-lg font-bold transition-all duration-200 ease-in-out w-25">Projects</button></a></li>
-                <li><a href=""><button className="hover:text-[white] hover:text-xl hover:bg-[#073761] bg-[#081b2e] cursor-pointer lg:rounded-xl rounded-md h-8 lg:w-30 lg:text-lg font-bold transition-all duration-200 ease-in-out w-25">Contact</button></a></li>
+        <div className="fixed z-10 top-1 w-[100%] flex justify-center"> 
+            <ul className="flex flex-row justify-center h-13 w-[85%] items-center bg-[#122b45] gap-5 rounded-2xl shadow-xl">
+                <li><a href=""><button className="hover:text-[white] hover:text-xl hover:bg-[#2a5a8c] bg-[#1b3d61] cursor-pointer lg:rounded-xl rounded-md h-8 md:w-25 lg:w-25 lg:text-lg font-bold transition-all duration-200 ease-in-out w-15">Home</button></a></li>
+                <li><a href=""><button className="hover:text-[white] hover:text-xl hover:bg-[#2a5a8c] bg-[#1b3d61] cursor-pointer lg:rounded-xl rounded-md h-8 md:w-25 lg:w-25 lg:text-lg font-bold transition-all duration-200 ease-in-out w-15">About</button></a></li>
+                <li><a href=""><button className="hover:text-[white] hover:text-xl hover:bg-[#2a5a8c] bg-[#1b3d61] cursor-pointer lg:rounded-xl rounded-md h-8 md:w-30 lg:w-30 lg:text-lg font-bold transition-all duration-200 ease-in-out w-15">Projects</button></a></li>
+                <li><a href=""><button className="hover:text-[white] hover:text-xl hover:bg-[#2a5a8c] bg-[#1b3d61] cursor-pointer lg:rounded-xl rounded-md h-8 md:w-30 lg:w-30 lg:text-lg font-bold transition-all duration-200 ease-in-out w-15">Contact</button></a></li>
 
             </ul>
         </div>
