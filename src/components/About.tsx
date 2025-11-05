45b5b9092730dd230ad1f8a0d38beb3168777de1
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2025-11-05 15:56:27 -0800

    Start responsive text size designing for about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,7 +13,7 @@ export default function About(){
             <div data-aos="zoom-in" className="bg-[#0b233b] p-10 rounded-xl flex flex-col shadow-2xl">
                 <h2 className=" flex text-lg md:text-2xl font-bold text-center bg-[#112840] mb-5 rounded-2xl h-15 justify-center items-center shadow-2xl p-3">Here's a little <span className="text-[#2289e3]">&nbsp;About Me:</span></h2>
                     <div className="flex flex-col md:flex-row justify-between gap-8">
-                        <div data-aos="zoom-in-right" className="flex justify-start text-md md:text-lg text-left bg-[#112840] rounded-2xl p-4 hidden md:flex shadow-2xl flex-1 max-w-110">
+                        <div data-aos="zoom-in-right" className="flex justify-start text-md md:text-md lg:text-lg xl:text-lg text-left bg-[#112840] rounded-2xl p-4 hidden md:flex shadow-2xl flex-1 max-w-110">
                             I'm passionate about artificial intelligence and natural language processing. There's something fascinating about teaching machines to understand and generate human
                             language—it's like building a bridge between human thought and computational power.
                             Right now, I'm working on two RAG projects that I'm really excited about. RTFM helps communities avoid answering the same questions over and over by retrieving previous 
